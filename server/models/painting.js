682b2d14d9d184899785a065f2d489cc5d9f480f
oAmirrezao <119684561+oAmirrezao@users.noreply.github.com>
2025-08-01 23:36:25 +0330

    Update painting.js

--- server/models/painting.js
+++ server/models/painting.js
@@ -2,6 +2,6 @@ module.exports = (sequelize) => {
   const { DataTypes } = require('sequelize');
   return sequelize.define('Painting', {
     title: { type: DataTypes.STRING, allowNull: false },
-    data:  { type: DataTypes.TEXT, allowNull: false }  // JSON.stringify نقاشی
+    data:  { type: DataTypes.TEXT, allowNull: false }
   });
 };
